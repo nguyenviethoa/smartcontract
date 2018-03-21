0df19b4f9448f2159545e0e8866e67de299e3f78
viethoa <nguyenviethoa1984@gmail.com>
2018-03-21 18:43:37 +0700

    fix version web3

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,7 +1,9 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const { interface, bytecode } = require('../compile');
 
 let accounts;
@@ -12,11 +14,18 @@ beforeEach(async () => {
 
     inbox = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode, arguments: ['Hi there'] })
-        .send({ from: accounts[0], gas: '100000' });
+        .send({ from: accounts[0], gas: '1000000' });
+
+    inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
     it('deploys contracts', () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address)
     });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hi there');
+    })
 });
